a43c20e131120b0557961e7ef1754afe345fbc12
Maxwell <mxt.developer@gmail.com>
2021-03-06 02:42:00 +0200

    Add dendencies for initial annotation processors work

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -1,7 +1,14 @@
+import co.anitrend.support.query.builder.buildSrc.Libraries
+
 plugins {
 	id("co.anitrend.support.query.builder.plugin")
 }
 
 dependencies {
+	//implementation(project(":annotations"))
 
+	implementation(Libraries.Google.AutoService.autoService)
+	kapt(Libraries.Google.AutoService.autoService)
+
+	implementation(Libraries.Square.KotlinPoet.kotlinPoet)
 }
