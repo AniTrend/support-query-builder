a55429d933554d775e4788e54919e56f80fee6b7
Maxwell <mxt.developer@gmail.com>
2021-03-07 20:00:46 +0200

    Add annotation class documentation

--- annotations/src/main/kotlin/co/anitrend/support/query/builder/annotation/EntityAnnotations.kt
+++ annotations/src/main/kotlin/co/anitrend/support/query/builder/annotation/EntityAnnotations.kt
@@ -1,5 +1,9 @@
 package co.anitrend.support.query.builder.annotation
 
+/**
+ * Marks a class as an entity schema. This will create a file with the room
+ * annotations on table name, columns and embeds
+ */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 annotation class EntitySchema
