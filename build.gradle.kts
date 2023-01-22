16fda85722bada46d1a2ed3aad93d401bee7fdc2
Maxwell <mxt.developer@gmail.com>
2023-01-22 12:58:06 +0200

    Add plugin management to settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,11 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import co.anitrend.support.query.builder.buildSrc.plugins.resolver.handleDependencySelection
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
     id("com.github.ben-manes.versions")
 }
 
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
     dependencies {
         classpath(libs.android.gradle.plugin)
         classpath(libs.jetbrains.kotlin.gradle)
